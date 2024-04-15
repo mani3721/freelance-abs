6460749af4c849dd95f0732f6f124cb621f801c4
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2024-04-15 16:43:58 +0530

    clone
    Signed-off-by: Gopisakthivel <gopisakthivel@alphabsolutions.com>

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 
+    // template
     const template = $('.template');
-
     for (let i = 0; i < 3; i++) {
         const clone = template.contents().clone();
         $('.feature-card-container').append(clone);;
     }
 
+    // accordion
     $('.accordion-header').click(function(){
         $('.accordion-description').not($(this).next('.accordion-description')).slideUp();
         $(this).next(".accordion-description").slideToggle();
